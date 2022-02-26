e9931cbd0fc09262f404687dc363114d878c7dee
Zihao Yu <yuzihao@ict.ac.cn>
2022-02-26 15:47:42 +0800

    example,vsrc,seg: display digits 0~7 to keep consistent with indices

--- example/vsrc/seg.v
+++ example/vsrc/seg.v
@@ -12,14 +12,14 @@ module seg(
 );
 
 wire [7:0] segs [7:0];
-assign segs[0] = 8'b01100001;
-assign segs[1] = 8'b11011010;
-assign segs[2] = 8'b11110010;
-assign segs[3] = 8'b01100110;
-assign segs[4] = 8'b10110110;
-assign segs[5] = 8'b10111110;
-assign segs[6] = 8'b11100000;
-assign segs[7] = 8'b11111110;
+assign segs[0] = 8'b11111101;
+assign segs[1] = 8'b01100000;
+assign segs[2] = 8'b11011010;
+assign segs[3] = 8'b11110010;
+assign segs[4] = 8'b01100110;
+assign segs[5] = 8'b10110110;
+assign segs[6] = 8'b10111110;
+assign segs[7] = 8'b11100000;
 
 parameter CLK_NUM = 5000000;
 
