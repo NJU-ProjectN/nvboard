45530c16b9dd52ebf0317faa8f2632371faeb0b2
nekomona <nekomona@163.com>
2022-03-15 13:37:02 +0800

    Interface positioning

--- include/render.h
+++ include/render.h
@@ -6,38 +6,38 @@
 #define WINDOW_WIDTH   640
 #define WINDOW_HEIGHT  480
 
-#define SWITCH_X       80
+#define SWITCH_X       60
 #define SWITCH_Y       400
 #define SWITCH_SEP     10
 #define SWITCH_WIDTH   20
 #define SWITCH_HEIGHT  40
 
-#define BTNC_X         500
-#define BTNC_Y         200
+#define BTNC_X         520
+#define BTNC_Y         250
 #define BTNC_SEP       20
-#define BTNC_WIDTH     20
-#define BTNC_HEIGHT    20
+#define BTNC_WIDTH     30
+#define BTNC_HEIGHT    30
 
-#define LED_X          85
+#define LED_X          60 + (4/2)
 #define LED_Y          360
-#define LED_SEP        20
-#define LED_WIDTH      10
-#define LED_HEIGHT     4
+#define LED_SEP        14
+#define LED_WIDTH      16
+#define LED_HEIGHT     8
 
-#define SEG_X          40
-#define SEG_Y          240
+#define SEG_X          60
+#define SEG_Y          225
 
 #define SEG_VER_WIDTH  3
 #define SEG_VER_HEIGHT 30
 #define SEG_HOR_WIDTH  30
 #define SEG_HOR_HEIGHT 3
-#define SEG_DOT_WIDTH  SEG_VER_WIDTH
-#define SEG_DOT_HEIGHT SEG_HOR_HEIGHT
+#define SEG_DOT_WIDTH  4
+#define SEG_DOT_HEIGHT 4
 
 #define SEG_SEP        3
 
-#define SEG_TOT_WIDTH  (SEG_SEP * 17 + SEG_VER_WIDTH * 24 + SEG_HOR_WIDTH * 8)
-#define SEG_TOT_HEIGHT (SEG_SEP * 2 + SEG_VER_HEIGHT * 2 + SEG_HOR_HEIGHT * 3)
+#define SEG_TOT_WIDTH  (SEG_SEP * 18 + SEG_VER_WIDTH * 16 + SEG_DOT_WIDTH * 8 + SEG_HOR_WIDTH * 8)
+#define SEG_TOT_HEIGHT (SEG_SEP * 4 + SEG_VER_HEIGHT * 2 + SEG_HOR_HEIGHT * 3)
 
 void load_background(SDL_Renderer *renderer);
 
